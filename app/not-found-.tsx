64b59a87c0aa92688ea241d462990d9cd9334524
khushijai20 <khushijai841221@gmail.com>
2025-09-16 10:24:23 +0530

    Updated project changes

--- app/not-found-.tsx
+++ app/not-found-.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100 text-gray-800">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-xl mb-6">Oops! Page not found 😢</p>
-      <a
+      <Link
         href="/"
         className="px-6 py-3 bg-gradient-to-r from-pink-500 to-purple-500 text-white rounded-lg shadow hover:opacity-90"
       >
         Go Back Home
-      </a>
+      </Link>
     </div>
   );
 }
